5e17e21f8caaf779e759195d2683138e3c0dfb3c
consuali <consuali@gmail.com>
2020-11-19 16:00:00 +0100

    creato primo component con index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexCounterComponent } from './index-counter/index-counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexCounterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/index-counter/index-counter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-index-counter',
+  templateUrl: './index-counter.component.html',
+  styleUrls: ['./index-counter.component.css']
+})
+export class IndexCounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
