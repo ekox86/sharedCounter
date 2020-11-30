4b72830c3e0281d2f24aac82385c1a2b912a5499
consuali <consuali@gmail.com>
2020-11-30 16:45:47 +0100

    aggiunte funzionalità su ts

--- src/app/index-counter/index-counter.component.ts
+++ src/app/index-counter/index-counter.component.ts
@@ -26,5 +26,31 @@ export class IndexCounterComponent implements OnInit {
   reset(){
     this.contatore=0;
   }
+
+  sottDieci(){
+    this.contatore = this.contatore -10;
+    
+  }
+
+  sottVenti(){
+    this.contatore = this.contatore -20;
+  }
+
+  sottCento(){
+    this.contatore = this.contatore -100;
+  }
+
+  add10(){
+    this.contatore = this.contatore +10;
+  }
   
+  add20(){
+    this.contatore = this.contatore +20;
+  }
+
+  add100(){
+   
+      this.contatore = this.contatore +100;
+    
+  }
 }
